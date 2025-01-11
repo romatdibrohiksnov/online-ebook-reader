46c92780f4b855b4528577ce056c5922f9351e49
romatdi <romatdibrohiksnov@gmail.com>
2025-01-11 12:01:44 +0800

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="m-auto w-full py-3 px-4 sm:px-6 mx-auto mt-8">
     <div class="flex justify-between items-center gap-5">
-      <p class="text-xs">Made in China <a href="#" target="_blank" class="underline">一个热心网友</a>.</p>
+      <p class="text-xs">Made in China <a href="#" target="_blank" class="underline">paradox</a>.</p>
       <p class="text-xs"><a href="https://github.com/romatdibrohiksnov/online-ebook-reader" target="_blank" class="underline">Github</a></p>
     </div>
   </footer>
