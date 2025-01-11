7462c519e9b29aa55b2a56b252bc00ca04a06c0c
romatdi <romatdibrohiksnov@gmail.com>
2025-01-11 11:46:25 +0800

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="m-auto w-full py-3 px-4 sm:px-6 mx-auto mt-8">
     <div class="flex justify-between items-center gap-5">
-      <p class="text-xs">Made in Manchester by <a href="https://www.kevan.tv" target="_blank" class="underline">Matt Kevan</a>.</p>
-      <p class="text-xs"><a href="https://github.com/MattKevan/minimal-reader/" target="_blank" class="underline">Github</a></p>
+      <p class="text-xs">Made in China" target="_blank" class="underline">一个热心网友</a>.</p>
+      <p class="text-xs"><a href="https://github.com/romatdibrohiksnov/online-ebook-reader" target="_blank" class="underline">Github</a></p>
     </div>
   </footer>
 </template>
