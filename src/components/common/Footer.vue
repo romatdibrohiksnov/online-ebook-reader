b219eb5f22f2a44e98c0e1922b4aeeeebff6a158
romatdi <romatdibrohiksnov@gmail.com>
2025-01-11 11:56:39 +0800

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="m-auto w-full py-3 px-4 sm:px-6 mx-auto mt-8">
     <div class="flex justify-between items-center gap-5">
-      <p class="text-xs">Made in China" target="_blank" class="underline">一个热心网友</a>.</p>
+      <p class="text-xs">Made in China <a href="#" target="_blank" class="underline">一个热心网友</a>.</p>
       <p class="text-xs"><a href="https://github.com/romatdibrohiksnov/online-ebook-reader" target="_blank" class="underline">Github</a></p>
     </div>
   </footer>
 </template>
-<script>
 
+<script>
 export default {
   name: 'AppFooter'
 }
